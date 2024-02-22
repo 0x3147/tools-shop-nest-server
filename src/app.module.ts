ddb7ec6fef7ba166ec261a35b45d8a8a43a85af4
radeonvii <kjx01771103@cainiao.com>
2024-02-22 17:27:37 +0800

    feat: winston日志引入

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { connectionParams } from '../ormconfig'
+import { WinstonModule } from './common/winston.module'
 import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionParams), UserModule],
+  imports: [
+    TypeOrmModule.forRoot(connectionParams),
+    WinstonModule.forRoot(),
+    UserModule
+  ],
   controllers: [],
   providers: []
 })
