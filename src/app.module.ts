e5e042f48a4c9aaaeb67abdc8b4e2cb8bfef7e3a
radeonvii <kjx01771103@cainiao.com>
2024-02-22 17:05:40 +0800

    feat: user模块配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { connectionParams } from '../ormconfig'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionParams)],
+  imports: [TypeOrmModule.forRoot(connectionParams), UserModule],
   controllers: [],
   providers: []
 })
---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,54 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from 'typeorm'
+
+@Entity({
+  name: 'tools_shop_user'
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({
+    comment: '业务id',
+    type: 'bigint'
+  })
+  postId: number
+
+  @Column({
+    comment: '用户名',
+    type: 'varchar',
+    length: 50
+  })
+  username: string
+
+  @Column({
+    comment: '密码',
+    type: 'varchar',
+    length: 255
+  })
+  password: string
+
+  @Column({
+    comment: '邮箱',
+    type: 'varchar',
+    length: 255
+  })
+  email: string
+
+  @Column({
+    comment: '是否管理员',
+    default: false
+  })
+  isAdmin: boolean
+
+  @CreateDateColumn()
+  createTime: Date
+
+  @UpdateDateColumn()
+  updateTime: Date
+}
