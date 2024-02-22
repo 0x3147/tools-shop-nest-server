c794bd8ac19c55155620690f8f43fec96d1d54b4
radeonvii <kjx01771103@cainiao.com>
2024-02-22 17:28:48 +0800

    feat: winston日志引入

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ResponseInterceptor } from './interceptor/response.interceptor'
+import { WINSTON_LOGGER_TOKEN } from './common/winston.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.useLogger(app.get(WINSTON_LOGGER_TOKEN));
 
   app.useGlobalPipes(new ValidationPipe())
 
