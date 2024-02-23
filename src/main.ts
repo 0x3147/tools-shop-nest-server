6e4c43730361f6c125db301ebf952050ab557b85
radeonvii <kjx01771103@cainiao.com>
2024-02-23 16:26:18 +0800

    feat: nest跨域配置

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,32 @@
 import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ResponseInterceptor } from './interceptor/response.interceptor'
 import { WINSTON_LOGGER_TOKEN } from './common/winston.module'
+import { ResponseInterceptor } from './interceptor/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
 
-  app.useLogger(app.get(WINSTON_LOGGER_TOKEN));
+  const allowedOrigins = configService.get('ALLOWED_ORIGINS').split(',')
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'), false)
+      }
+    }
+  })
+
+  app.useLogger(app.get(WINSTON_LOGGER_TOKEN))
 
   app.useGlobalPipes(new ValidationPipe())
 
   app.useGlobalInterceptors(new ResponseInterceptor())
 
-  await app.listen(3000)
+  await app.listen(6666)
 }
 bootstrap()
