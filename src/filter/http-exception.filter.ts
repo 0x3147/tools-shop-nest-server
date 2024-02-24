a8817d50e1b34340dc35e599ae1085b6f963f1fb
radeonvii <radeonk@foxmail.com>
2024-02-24 13:31:21 +0800

    feat: 登录守卫引入、自定义装饰器

--- src/filter/http-exception.filter.ts
+++ src/filter/http-exception.filter.ts
@@ -4,15 +4,22 @@ import {
   ExceptionFilter,
   HttpException
 } from '@nestjs/common'
+import { Response } from 'express'
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp()
-    const response = ctx.getResponse()
-    const exceptionResponse = exception.getResponse()
+    const response = ctx.getResponse<Response>()
+    const status = exception.getStatus()
+
+    const res = exception.getResponse() as { message: string[] }
 
     // 直接返回自定义异常中的内容
-    response.status(exception.getStatus()).json(exceptionResponse)
+    response.status(status).json({
+      statusCode: status,
+      success: false,
+      message: res?.message?.join ? res?.message.join(',') : exception.message
+    })
   }
 }
