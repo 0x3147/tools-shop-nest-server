1c5315c9b0aa4adc9413505e07026bec1f37c9af
radeonvii <kjx01771103@cainiao.com>
2024-03-06 11:40:40 +0800

    feat: 剔除支付模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { connectionParams } from '../ormconfig'
 import { WinstonModule } from './common/winston.module'
 import { EmailModule } from './email/email.module'
 import { LoginGuard } from './guard/login.guard'
-import { PaymentModule } from './payment/payment.module'
 import { RedisModule } from './redis/redis.module'
 import { SnowFlakeModule } from './snow-flake/snow-flake.module'
 import { UserModule } from './user/user.module'
@@ -34,8 +33,7 @@ import { UserModule } from './user/user.module'
     UserModule,
     SnowFlakeModule,
     EmailModule,
-    RedisModule,
-    PaymentModule
+    RedisModule
   ],
   providers: [
     {
