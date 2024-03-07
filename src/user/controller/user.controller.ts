fb35fbab05f4c2ad404016fd8ff4817c30696fbe
radeonvii <radeonk@foxmail.com>
2024-03-08 00:28:00 +0800

    feat: 普通用户查询

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -7,6 +7,7 @@ import { EmailService } from '../../email/email.service'
 import { RedisService } from '../../redis/redis.service'
 import { ForgetPasswordDto } from '../dto/forget-password.dto'
 import { LoginUserDto } from '../dto/login-user.dto'
+import { QueryCommonUserDto } from '../dto/query-common-user.dto'
 import { RegisterUserDto } from '../dto/register-user.dto'
 import { UpdateUserInfoDto } from '../dto/update-user-info.dto'
 import { UpdateUserPasswordDto } from '../dto/update-user-password.dto'
@@ -143,5 +144,10 @@ export class UserController {
   @Post('forget')
   async forgetPassword(@Body() forgetPasswordDto: ForgetPasswordDto) {
     return await this.userService.forgetPassword(forgetPasswordDto)
+  }
+
+  @Post('query-common-user')
+  async queryCommonUser(@Body() queryCommonUserDto: QueryCommonUserDto) {
+    return await this.userService.queryCommonUser(queryCommonUserDto)
   }
 }
--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -14,6 +14,7 @@ import { SnowFlakeService } from '../../snow-flake/snow-flake.service'
 import { handleDecrypt, handleEncrypt } from '../../util/argon2Util'
 import { ForgetPasswordDto } from '../dto/forget-password.dto'
 import { LoginUserDto } from '../dto/login-user.dto'
+import { QueryCommonUserDto } from '../dto/query-common-user.dto'
 import { RegisterUserDto } from '../dto/register-user.dto'
 import { UpdateUserInfoDto } from '../dto/update-user-info.dto'
 import { UpdateUserPasswordDto } from '../dto/update-user-password.dto'
@@ -21,6 +22,7 @@ import { Permission } from '../entity/permission.entity'
 import { Role } from '../entity/role.entity'
 import { User } from '../entity/user.entity'
 import { LoginUserVo } from '../resp/login-vo'
+import { QueryUserListVo } from '../resp/query-user-list.vo'
 import { UserDetailVo } from '../resp/user-info.vo'
 
 @Injectable()
@@ -289,5 +291,53 @@ export class UserService {
         ToolsShopExceptionEnumDesc.UPDATE_PASSWORD_FAIL
       )
     }
+  }
+
+  async queryCommonUser(queryCommonUserDto: QueryCommonUserDto) {
+    const {
+      pageSize = 1,
+      currentPage = 10,
+      username = undefined,
+      email = undefined
+    } = queryCommonUserDto
+
+    const skip = (currentPage - 1) * pageSize
+
+    let query = this.userRepository
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.roles', 'role')
+      .where('user.isAdmin = :isAdmin', { isAdmin: false })
+      .andWhere('role.name = :roleName', { roleName: '普通用户' })
+
+    if (username) {
+      query = query.andWhere('user.username LIKE :username', {
+        username: `%${username}%`
+      })
+    }
+
+    if (email) {
+      query = query.andWhere('user.email LIKE :email', { email: `%${email}%` })
+    }
+
+    const [result, total] = await query
+      .skip(skip)
+      .take(pageSize)
+      .getManyAndCount()
+
+    const tableData = result.map((user) => ({
+      postId: user.postId,
+      username: user.username,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      isFrozen: user.isFrozen,
+      createTime: user.createTime
+    }))
+
+    const resp = new QueryUserListVo()
+    resp.tableData = tableData
+    resp.total = total
+    resp.currentPage = currentPage
+
+    return resp
   }
 }
