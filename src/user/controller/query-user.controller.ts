749668a902310f5a05cda128dac58aaa13609e3e
radeonvii <kjx01771103@cainiao.com>
2024-03-08 09:32:28 +0800

    feat: 查询权限添加

--- src/user/controller/query-user.controller.ts
+++ src/user/controller/query-user.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common'
+import { RequireLogin, RequirePermission } from '../../common/custom.decorator'
+import { PermissionCode } from '../../common/permission'
 import { QueryCommonUserDto } from '../dto/query-common-user.dto'
 import { UserService } from '../service/user.service'
 
@@ -8,6 +10,8 @@ export class QueryUserController {
   private userService: UserService
 
   @Post('common')
+  @RequireLogin()
+  @RequirePermission(PermissionCode.HAVE_ALL_PERMISSIONS)
   async queryCommonUser(@Body() queryCommonUserDto: QueryCommonUserDto) {
     return await this.userService.queryCommonUser(queryCommonUserDto)
   }
