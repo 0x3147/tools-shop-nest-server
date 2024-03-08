77dde725a3ebe5a9cc21a6bc1a037b50d4ca8ee2
radeonvii <kjx01771103@cainiao.com>
2024-03-08 13:28:24 +0800

    feat: 查询条件优化

--- src/user/service/user-query.service.ts
+++ src/user/service/user-query.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { QueryCommonUserDto } from '../dto/query-common-user.dto'
-import { Permission } from '../entity/permission.entity'
-import { Role } from '../entity/role.entity'
 import { User } from '../entity/user.entity'
 import { QueryUserListVo } from '../resp/query-user-list.vo'
 
@@ -12,35 +10,36 @@ export class UserQueryService {
   @InjectRepository(User)
   private userRepository: Repository<User>
 
-  @InjectRepository(Role)
-  private roleRepository: Repository<Role>
-
-  @InjectRepository(Permission)
-  private permissionRepository: Repository<Permission>
-
-  async queryCommonUser(queryCommonUserDto: QueryCommonUserDto) {
-    const {
-      pageSize = 1,
-      currentPage = 10,
-      username = undefined,
-      email = undefined
-    } = queryCommonUserDto
-
+  async makeQuery(
+    isAdmin: boolean,
+    pageSize: number = 1,
+    currentPage: number = 1,
+    username: string | undefined,
+    email: string | undefined,
+    type?: 'common' | 'member'
+  ) {
     const skip = (currentPage - 1) * pageSize
 
     let query = this.userRepository
       .createQueryBuilder('user')
       .leftJoinAndSelect('user.roles', 'role')
-      .where('user.isAdmin = :isAdmin', { isAdmin: false })
-      .andWhere('role.name = :roleName', { roleName: '普通用户' })
+      .where('user.isAdmin = :isAdmin', { isAdmin })
 
-    if (username) {
+    if (type === 'common') {
+      query = query.andWhere('role.name = :roleName', { roleName: '普通用户' })
+    }
+
+    if (type === 'member') {
+      query = query.andWhere('role.name = :roleName', { roleName: '会员用户' })
+    }
+
+    if (username !== undefined && username !== '') {
       query = query.andWhere('user.username LIKE :username', {
         username: `%${username}%`
       })
     }
 
-    if (email) {
+    if (email !== undefined && email !== '') {
       query = query.andWhere('user.email LIKE :email', { email: `%${email}%` })
     }
 
@@ -65,6 +64,24 @@ export class UserQueryService {
     return resp
   }
 
+  async queryCommonUser(queryCommonUserDto: QueryCommonUserDto) {
+    const {
+      pageSize = 1,
+      currentPage = 10,
+      username = undefined,
+      email = undefined
+    } = queryCommonUserDto
+
+    return await this.makeQuery(
+      false,
+      pageSize,
+      currentPage,
+      username,
+      email,
+      'common'
+    )
+  }
+
   async queryAdminUser(queryCommonUserDto: QueryCommonUserDto) {
     const {
       pageSize = 1,
@@ -73,41 +90,6 @@ export class UserQueryService {
       email = undefined
     } = queryCommonUserDto
 
-    const skip = (currentPage - 1) * pageSize
-
-    let query = this.userRepository
-      .createQueryBuilder('user')
-      .leftJoinAndSelect('user.roles', 'role')
-      .where('user.isAdmin = :isAdmin', { isAdmin: true })
-
-    if (username) {
-      query = query.andWhere('user.username LIKE :username', {
-        username: `%${username}%`
-      })
-    }
-
-    if (email) {
-      query = query.andWhere('user.email LIKE :email', { email: `%${email}%` })
-    }
-
-    const [result, total] = await query
-      .skip(skip)
-      .take(pageSize)
-      .getManyAndCount()
-
-    const tableData = result.map((user) => ({
-      postId: user.postId,
-      username: user.username,
-      email: user.email,
-      isFrozen: user.isFrozen,
-      createTime: user.createTime
-    }))
-
-    const resp = new QueryUserListVo()
-    resp.tableData = tableData
-    resp.total = total
-    resp.currentPage = currentPage
-
-    return resp
+    return await this.makeQuery(true, pageSize, currentPage, username, email)
   }
 }
