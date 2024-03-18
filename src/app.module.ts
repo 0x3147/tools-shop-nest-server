41a9357dcda916905640400d6a064e3374bf209d
radeonvii <kjx01771103@cainiao.com>
2024-03-18 13:35:53 +0800

    feat: 商品模块引入

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,10 @@ import { EmailModule } from './email/email.module'
 import { LoginGuard } from './guard/login.guard'
 import { PermissionsGuard } from './guard/permission.guard'
 import { OssModule } from './oss/oss.module'
+import { ProductionModule } from './production/production.module'
 import { RedisModule } from './redis/redis.module'
 import { SnowFlakeModule } from './snow-flake/snow-flake.module'
 import { UserModule } from './user/user.module'
-import { ProductionModule } from './production/production.module';
 
 @Module({
   imports: [
