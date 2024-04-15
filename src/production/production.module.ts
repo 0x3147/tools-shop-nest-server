46fc9b5b5d966755b71bd48c8c7a8a7b777b32da
radeonvii <kjx01771103@cainiao.com>
2024-04-15 14:19:33 +0800

    feat: 商品模块添加

--- src/production/production.module.ts
+++ src/production/production.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { OssService } from '../oss/oss.service'
+import { Product } from './entity/product.entity'
 import { ProductionController } from './production.controller'
 import { ProductionService } from './production.service'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductionController],
-  providers: [ProductionService]
+  providers: [ProductionService, OssService]
 })
 export class ProductionModule {}
