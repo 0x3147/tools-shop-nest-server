cb2b0bc4e93252a721cca7b746812645ab7353d9
radeonvii <kjx01771103@cainiao.com>
2024-04-22 17:32:54 +0800

    feat: tag服务添加

--- src/production/production.module.ts
+++ src/production/production.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { OssService } from '../oss/oss.service'
 import { ProductionController } from './controller/production.controller'
+import { TagController } from './controller/tag.controller'
 import { Product } from './entity/product.entity'
 import { Tag } from './entity/tag.entity'
 import { ProductionService } from './service/production.service'
+import { TagService } from './service/tag.service'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product, Tag])],
-  controllers: [ProductionController],
-  providers: [ProductionService, OssService]
+  controllers: [ProductionController, TagController],
+  providers: [ProductionService, TagService, OssService]
 })
 export class ProductionModule {}
